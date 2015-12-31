6963a4cee02d86c72a77884f9039e5a7db48556f
mirrorecho <info@randallwest.com>
2015-12-30 22:06:34 -0600

    ya

--- test.py
+++ test.py
@@ -1,4 +1,24 @@
 from abjad import *
+import ly.document
+
+ly_string = ""
+
+with open ("test.ly", "r") as ly_file:
+    ly_string = ly_file.read()
+
+d = ly.document.Document(ly_string)
+
+print(d.block("ya").text())
+
+# l = ly_string.split("=")
+
+# for i, x in enumerate(l):
+#     if i % 2 == 0:
+#         print(i)
+#         print(x)
+
+
+# print(l)
 
 
 # c = Container()
@@ -12,10 +32,10 @@ from abjad import *
 
 # print(c.select_leaves(allow_discontiguous_leaves=True))
 
-c = Container("b4 << {b2} \\\\  {\\voiceTwo a8([ c8]) r4} >> r4")
+# c = Container("b4 << {b2} \\\\  {\\voiceTwo a8([ c8]) r4} >> r4")
 
-s = Staff()
-s.append(c)
+# s = Staff()
+# s.append(c)
 
-show(s)
-print(format(s))
+# show(s)
+# print(format(s))
