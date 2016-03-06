3eb84e6378a9ef8ee3c168b0d42f84f8059ef816
mirrorecho <info@randallwest.com>
2016-03-06 14:36:58 -0600

    yo

--- _settings.py
+++ _settings.py
@@ -1,11 +1,10 @@
 import sys
 
-# replace with local path...
-ROOT_PATH = "/home/randall/Code/mirrorecho" 
+import sys
+
+ROOT_PATH = "/Users/randallwest/Code/mirrorecho" 
 sys.path.append(ROOT_PATH)
 
-# replace with project name...
+# project path and settings:
 PROJECT_NAME = "rwestmusic-drops-of-neon"
-
-# relpace local path / project name...
-PROJECT_PATH = "/home/randall/Code/mirrorecho/rwestmusic-drops-of-neon"
+PROJECT_PATH = "/Users/randallwest/Code/mirrorecho/rwestmusic-drops-of-neon"
